2ccfb3ec2a2b98193cbe61efcc78b3257ba963ac
Luca Schimweg <luca.schimweg@sap.com>
2021-02-27 00:21:06 +0100

    Add API types

---
+++ Shared/API/types.swift
@@ -0,0 +1,60 @@
+//
+//  api:types.swift
+//  MobilMitStil
+//
+//  Created by Schimweg, Luca on 26/02/2021.
+//
+
+import Foundation
+
+struct Location {
+    var latitude: Double
+    var longitude: Double
+}
+
+struct RidePreferences {
+    var smoker: Bool
+    var children: Bool
+}
+
+struct RideConfig {
+    var location: Location
+    var seats: Int
+    var preferences: RidePreferences
+}
+
+typealias SessionId = String;
+
+typealias Uuid = String;
+
+struct DriverInformation {
+    var userId: Uuid
+    var pickupPoint: Location
+    var dropoffPoint: Location
+    var requested: Bool
+}
+
+struct Estimation {
+    var userId: Uuid
+    var pickupTime: Int
+    var destinationTime: Int
+}
+
+enum AcceptanceStatus {
+    case ACCEPTED
+    case DECLINED
+}
+
+struct Confirmation {
+    userId: Uuid
+    status: AcceptanceStatus
+}
+
+
+struct PassengerRequest {
+    var location: Location
+    var destination: Location
+    var tolerance: Int
+    var requestedSeats: Int
+    var preferences: RidePreferences
+}
