5d0dc8d0e2fe1dc2b813e6c2040aad85ccba8bf7
Michael Spilger <mispilger@gmail.com>
2021-02-26 21:58:06 +0100

    Feat: Inital Project Setup

---
+++ MobilMitStil/Shared/MobilMitStilApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobilMitStilApp.swift
+//  Shared
+//
+//  Created by Michael Spilger on 26.02.21.
+//
+
+import SwiftUI
+
+@main
+struct MobilMitStilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
