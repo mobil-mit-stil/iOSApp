3bba405f63bd22e2a891b4ea6986699f64feb113
Michael Spilger <mispilger@gmail.com>
2021-02-27 00:51:52 +0100

    feat: ChooseDriverView

---
+++ Shared/ChooseDriverView.swift
@@ -0,0 +1,32 @@
+//
+//  ChooseDriverView.swift
+//  MobilMitStil
+//
+//  Created by Michael Spilger on 27.02.21.
+//
+
+import SwiftUI
+
+struct ChooseDriverView: View {
+    var body: some View {
+        ZStack {
+            ScrollView {
+                VStack {
+                    DriverView(name: "Marc Jakob", eta: 19, latitude: 49.2939547, longitude: 8.6405714).padding()
+                    DriverView(name: "Duc Vo Nogc", eta: 13, latitude: 51.507222, longitude: -0.1275).padding()
+                    DriverView(name: "Dustin Ramseger", eta: 9, latitude: 49.4892913, longitude: 8.4673098).padding()
+                    DriverView(name: "Luca Schimweg", eta: 4, latitude: 48.7784485, longitude: 9.1800132).padding()
+                    Text("Searching for drivers...").font(.largeTitle).padding()
+                }
+            }
+        }
+        .background(Color("Background"))
+        .ignoresSafeArea()
+    }
+}
+
+struct ChooseDriverView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChooseDriverView()
+    }
+}
