6d2b6c25a5d7a5411548d1f2617f5259a78e7e81
Michael Spilger <mispilger@gmail.com>
2021-02-27 01:35:44 +0100

    Feat: DirectionsView

---
+++ Shared/NavigationDirectionsView.swift
@@ -0,0 +1,32 @@
+//
+//  NavigationDirectionsView.swift
+//  MobilMitStil
+//
+//  Created by Michael Spilger on 27.02.21.
+//
+
+import SwiftUI
+
+struct NavigationDirectionsView: View {
+    
+    init(direction: String) {
+        self._direction = State(initialValue: direction)
+    }
+    @State var direction: String = ""
+    
+    var body: some View {
+        VStack {
+            Text(direction).font(.largeTitle).padding(15)
+        }.frame(width: 400, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .background(Color("BackgroundTop"))
+        .cornerRadius(20)
+        .shadow(color: .gray, radius: 3, x: 3, y: 3)
+        
+    }
+}
+
+struct NavigationDirectionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationDirectionsView(direction: "Go to the left")
+    }
+}
